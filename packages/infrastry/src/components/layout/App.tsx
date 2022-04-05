36114eb232d77fb9c86c6419fcf5933ec2036837
Il Harper <afanyiyu@hotmail.com>
2022-04-06 02:39:39 +0800

    feat: add grid style + update `Main`

--- packages/infrastry/src/components/layout/App.tsx
+++ packages/infrastry/src/components/layout/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import { PropStyle, PropWithChildren } from '../../types'
+import { PropWithChildren, PropWithStyle } from '../../types'
 import { combineClassName } from '../../utils'
 
-export const App: React.FC<PropWithChildren<PropStyle>> = (props) => (
-  <div
-    className={combineClassName('inf-base', props.className)}
-    style={props.style}
-  >
-    {props.children}
-  </div>
-)
+export const App: React.FC<
+  PropWithChildren<PropWithStyle<{ block?: boolean }>>
+> = (props) => {
+  const className = props.block
+    ? combineClassName('inf-app', props.className)
+    : combineClassName('inf-app inf-full-vh', props.className)
+  return (
+    <div className={className} style={props.style}>
+      {props.children}
+    </div>
+  )
+}
 
 export const InfApp = App
