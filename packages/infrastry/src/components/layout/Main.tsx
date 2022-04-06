680090c9cf5e33194836f1cfa7d5bc0673f2ef3a
Il Harper <afanyiyu@hotmail.com>
2022-04-07 01:56:47 +0800

    feat: update Main component

--- packages/infrastry/src/components/layout/Main.tsx
+++ packages/infrastry/src/components/layout/Main.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { PropStyle, PropWithChildren } from '../../types'
+import { combineClassName } from '../../utils'
 
 export const Main: React.FC<PropWithChildren<PropStyle>> = (props) => (
-  <main className={props.className} style={props.style}>
+  <main
+    className={combineClassName('inf-main', props.className)}
+    style={props.style}
+  >
     {props.children}
   </main>
 )
+
+export const InfMain = Main
