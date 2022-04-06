3b8a6829cfbe78ceff36087b51864449c6c61021
Il Harper <afanyiyu@hotmail.com>
2022-04-07 02:04:36 +0800

    refactor: update expended react types

--- packages/infrastry/src/types/utils/react.ts
+++ packages/infrastry/src/types/utils/react.ts
@@ -1,20 +1,14 @@
-export type Children =
-  | React.ReactElement<any, any>
-  | React.ReactElement<any, any>[]
-  | {}
+import { ReactNode } from 'react'
 
-export type PropChildren = { children?: Children }
-export type PropWithChildren<T> = (T extends unknown[] ? T[number] : T) &
-  PropChildren
-
-export type PropStyle = {
+export type PropsStyle = {
   className?: string
   style?: React.CSSProperties
 }
-export type PropWithStyle<T> = (T extends unknown[] ? T[number] : T) & PropStyle
+export type PropsWithStyle<T> = (T extends unknown[] ? T[number] : T) &
+  PropsStyle
 
 export type OrElement<T> =
   | (T extends unknown[] ? T[number] : T)
-  | Children
+  | ReactNode
   | null
   | undefined
