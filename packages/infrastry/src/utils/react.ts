09597b5ce41cc17440aae355b461fd68103f31c5
Il Harper <afanyiyu@hotmail.com>
2022-04-11 23:04:51 +0800

    refactor: update `combineClassName()` method

--- packages/infrastry/src/utils/react.ts
+++ packages/infrastry/src/utils/react.ts
@@ -1,9 +1,4 @@
-export function combineClassName(...args: (string | undefined)[]): string {
-  let result = ''
-  for (const str of args) {
-    if (typeof str === 'undefined') continue
-    if (result !== '') result += ' '
-    result += str.trim()
-  }
-  return result
-}
+export const combineClassName = (
+  ...args: (string | undefined)[]
+): string | undefined =>
+  args.reduce((x, y) => (x || '') + ' ' + (y || ''))?.trim()
