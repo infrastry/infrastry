c6308e4e9da741de9a323b5362afc6a5fb72d742
Il Harper <afanyiyu@hotmail.com>
2022-04-13 03:16:59 +0800

    feat: add `parseStyleProp()` high-order func

--- packages/infrastry/src/utils/infProps.ts
+++ packages/infrastry/src/utils/infProps.ts
@@ -1,6 +1,24 @@
 import { PropsWithStyle } from '../types'
 import { combineClassName } from './props'
 
+const parseStyleProp =
+  <TTargetProp, TPropName = keyof TTargetProp>(
+    propName: TPropName,
+    defaultPropClassName: string
+  ): (<TComponentProps extends PropsWithStyle<TTargetProp>>(
+    props: TComponentProps,
+    propClassName?: string
+  ) => TComponentProps) =>
+  (props, propClassName) => {
+    const parsedProps = { ...props }
+    if ((parsedProps as any)[propName])
+      parsedProps.className = combineClassName(
+        parsedProps.className,
+        propClassName || defaultPropClassName
+      )
+    return parsedProps
+  }
+
 //#region plain
 
 export type PropsPlain = {
@@ -9,18 +27,7 @@ export type PropsPlain = {
 export type PropsWithPlain<T> = (T extends unknown[] ? T[number] : T) &
   PropsPlain
 
-export function parsePlain<T extends PropsWithStyle<PropsPlain>>(
-  props: T,
-  plainClassName = 'inf-plain'
-): T {
-  const parsedProps = { ...props }
-  if (parsedProps.plain)
-    parsedProps.className = combineClassName(
-      parsedProps.className,
-      plainClassName
-    )
-  return parsedProps
-}
+export const parsePlain = parseStyleProp<PropsPlain>('plain', 'inf-plain')
 
 //#endregion
 
@@ -30,5 +37,7 @@ export interface PropsMini {
   mini?: boolean
 }
 export type PropsWithMini<T> = (T extends unknown[] ? T[number] : T) & PropsMini
+
+export const parseMini = parseStyleProp<PropsMini>('mini', 'inf-mini')
 
 //#endregion
